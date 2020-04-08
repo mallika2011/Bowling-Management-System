844cf760be2276a2401eda10beae96a0f2847fd3
ENikhil <nikhil.e@research.iiit.ac.in>
2020-04-09 04:18:09 +0530

    Added pause/play

---
+++ code/out/production/code/codemr/1/html/html_report_code/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"code working set: {code}", "Number of Highly Problematic Classes":0, "Number of Entities":31, "Number of Problematic Classes":0, "Class Lines of Code":1465, "Number of External Packages":13, "Number of Packages":1, "Number of External Entities":57}
