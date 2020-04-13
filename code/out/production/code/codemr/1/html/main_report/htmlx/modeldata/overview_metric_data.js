681cc2b69c385abbd51e22554e2b938e5005fceb
Mallika <mallika2011@yahoo.com>
2020-04-13 16:45:48 +0530

    Removed codemr graphs

--- code/out/production/code/codemr/1/html/main_report/htmlx/modeldata/overview_metric_data.js
+++
@@ -1,1 +0,0 @@
-var CodeMR_ProjectMetrics = {"Project Name":"code", "Number of Highly Problematic Classes":0, "Number of Entities":31, "Number of Problematic Classes":0, "Class Lines of Code":1465, "Number of External Packages":18, "Number of Packages":1, "Number of External Entities":89}
--- code/out/production/code/codemr/code/html/html_report_code/htmlx/modeldata/overview_metric_data.js
+++
@@ -1,1 +0,0 @@
-var CodeMR_ProjectMetrics = {"Project Name":"code working set: {code}", "Number of Highly Problematic Classes":0, "Number of Entities":1, "Number of Problematic Classes":0, "Class Lines of Code":117, "Number of External Packages":9, "Number of Packages":1, "Number of External Entities":28}
